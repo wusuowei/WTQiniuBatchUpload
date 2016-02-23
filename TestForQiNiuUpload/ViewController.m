f3cf3ec961b02a556fe7e979afb141993b3407e9
wentianen <wte_work@163.com>
2016-02-23 12:21:13 +0800

    project init

--- TestForQiNiuUpload/ViewController.m
+++ TestForQiNiuUpload/ViewController.m
@@ -19,9 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
 }
 
 @end
