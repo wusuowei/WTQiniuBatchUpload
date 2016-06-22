0dde471da054f86570f080ca3461a14bba36ab12
wentianen <1206860151@qq.com>
2016-06-22 10:11:13 +0800

    打开AF上传

--- TestForQiNiuUpload/ViewController.m
+++ TestForQiNiuUpload/ViewController.m
@@ -31,8 +31,8 @@
         builder.timeoutInterval = 10;
     }];
     self.uploadManager = [QNUploadManager sharedInstanceWithConfiguration:configuration];
-//    self.httpManager = [AFHTTPSessionManager manager];
-//    self.sessionManager = [[AFURLSessionManager alloc] initWithSessionConfiguration:nil];
+    self.httpManager = [AFHTTPSessionManager manager];
+    self.sessionManager = [[AFURLSessionManager alloc] initWithSessionConfiguration:nil];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
@@ -52,42 +52,41 @@
             }
         } option:nil];
     }
-    NSLog(@"七牛上传文件");
 }
 
-//- (void)beginUpload {
-//    NSMutableArray *uploadTasks = [NSMutableArray array];
-//    NSString *path = [[NSBundle mainBundle] pathForResource:@"abc" ofType:@"jpg"];
-//    NSData *data = [NSData dataWithContentsOfFile:path];
-//    for (NSInteger i = 0; i < 1000; i++) {
-//        NSString *key = [NSString stringWithFormat:@"234642/2C24529B-3774-4827-BE33-2ED4DDBA8125/9EF9C203-6FF3-4CA0-926F-C30A0F9F5B1A%zd.jpeg", i];
-//        NSString *url = [NSString stringWithFormat:@"http://upload.qiniu.com?a=%zd", i];
-//        [uploadTasks addObject:[self uploadData:data key:key url:url]];
-//    }
-//}
-//
-//- (NSURLSessionUploadTask *)uploadData:(NSData *)data key:(NSString *)key url:(NSString *)url{
-//    NSDictionary *params = @{@"key": key,
-//                             @"token": @"a0h5P_uSX_opuu0veh0W3gBYxunaVmIjpgQur9BM:0frqrjlj_g2R53WJGEstqXIaM34=:eyJzY29wZSI6InFhLW1lZGljYWwtcHJpdmF0ZS1jb21tb24iLCJkZWFkbGluZSI6MTQ2NjM1NDY1MiwiY2FsbGJhY2tVcmwiOiJodHRwOi8vbWVkY2hhcnQucWEueGluZ3NodWxpbi5jb20vY2FzZWZvbGRlci1tZWRpY2FsL3Fpbml1Q2FsbGJhY2siLCJjYWxsYmFja0JvZHkiOiJuYW1lPSQoa2V5KVx1MDAyNmhhc2g9JChldGFnKSJ9"};
-//    NSMutableURLRequest *request = [self.httpManager.requestSerializer
-//                                    multipartFormRequestWithMethod:@"POST"
-//                                    URLString:url
-//                                    parameters:params
-//                                    constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-//                                        [formData appendPartWithFileData:data name:@"file" fileName:key mimeType:@"application/octet-stream"];
-//                                    }
-//                                    
-//                                    error:nil];
-//    request.timeoutInterval = 10;
-//    NSURLSessionUploadTask *uploadTask = [self.httpManager uploadTaskWithRequest:request fromData:nil progress:nil completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
-//        if (error == nil) {
-//            NSLog(@"文件上传成功");
-//        } else {
-//            NSLog(@"文件上传失败 -> %@", error.userInfo[@"NSErrorFailingURLStringKey"]);
-//        }
-//    }];
-//    [uploadTask resume];
-//    return uploadTask;
-//}
+- (void)beginUpload {
+    NSMutableArray *uploadTasks = [NSMutableArray array];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"abc" ofType:@"jpg"];
+    NSData *data = [NSData dataWithContentsOfFile:path];
+    for (NSInteger i = 0; i < 1000; i++) {
+        NSString *key = [NSString stringWithFormat:@"234642/2C24529B-3774-4827-BE33-2ED4DDBA8125/9EF9C203-6FF3-4CA0-926F-C30A0F9F5B1A%zd.jpeg", i];
+        NSString *url = [NSString stringWithFormat:@"http://upload.qiniu.com?a=%zd", i];
+        [uploadTasks addObject:[self uploadData:data key:key url:url]];
+    }
+}
+
+- (NSURLSessionUploadTask *)uploadData:(NSData *)data key:(NSString *)key url:(NSString *)url{
+    NSDictionary *params = @{@"key": key,
+                             @"token": @"a0h5P_uSX_opuu0veh0W3gBYxunaVmIjpgQur9BM:0frqrjlj_g2R53WJGEstqXIaM34=:eyJzY29wZSI6InFhLW1lZGljYWwtcHJpdmF0ZS1jb21tb24iLCJkZWFkbGluZSI6MTQ2NjM1NDY1MiwiY2FsbGJhY2tVcmwiOiJodHRwOi8vbWVkY2hhcnQucWEueGluZ3NodWxpbi5jb20vY2FzZWZvbGRlci1tZWRpY2FsL3Fpbml1Q2FsbGJhY2siLCJjYWxsYmFja0JvZHkiOiJuYW1lPSQoa2V5KVx1MDAyNmhhc2g9JChldGFnKSJ9"};
+    NSMutableURLRequest *request = [self.httpManager.requestSerializer
+                                    multipartFormRequestWithMethod:@"POST"
+                                    URLString:url
+                                    parameters:params
+                                    constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+                                        [formData appendPartWithFileData:data name:@"file" fileName:key mimeType:@"application/octet-stream"];
+                                    }
+                                    
+                                    error:nil];
+    request.timeoutInterval = 10;
+    NSURLSessionUploadTask *uploadTask = [self.httpManager uploadTaskWithRequest:request fromData:nil progress:nil completionHandler:^(NSURLResponse * _Nonnull response, id  _Nullable responseObject, NSError * _Nullable error) {
+        if (error == nil) {
+            NSLog(@"文件上传成功");
+        } else {
+            NSLog(@"文件上传失败 -> %@", error.userInfo[@"NSErrorFailingURLStringKey"]);
+        }
+    }];
+    [uploadTask resume];
+    return uploadTask;
+}
 
 @end
