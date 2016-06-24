8d90f46737468096f10fb5f3d37e7303404a6320
wentianen <1206860151@qq.com>
2016-06-24 11:50:11 +0800

    test

---
+++ TestForQiNiuUpload/WTOperation.h
@@ -0,0 +1,13 @@
+//
+//  WTOperation.h
+//  TestForQiNiuUpload
+//
+//  Created by 温天恩 on 16/6/22.
+//  Copyright © 2016年 温. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WTOperation : NSOperation
+
+@end
---
+++ TestForQiNiuUpload/WTOperation.m
@@ -0,0 +1,19 @@
+//
+//  WTOperation.m
+//  TestForQiNiuUpload
+//
+//  Created by 温天恩 on 16/6/22.
+//  Copyright © 2016年 温. All rights reserved.
+//
+
+#import "WTOperation.h"
+
+@implementation WTOperation
+
+- (void)start {
+    [super start];
+    
+    
+}
+
+@end
